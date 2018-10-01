ff59b9ea84290758af7c082ea3d560741cbf0314
vector-of-bool <vectorofbool@gmail.com>
2018-10-01 00:07:39 -0600

    0.1.0 Files

--- do-docker.ps1
+++
@@ -1,8 +0,0 @@
-& docker build -t pmm-tester .
-if ($LASTEXITCODE) {
-    throw "Failed to build container image"
-}
-
-$here = $PSScriptRoot
-$MountArgs = "-v$($here):/host/source", "-v$($here)/build/in-docker:/host/build"
-& docker run @MountArgs -ti --rm pmm-tester cmake -H/host/source -B/host/build -GNinja
