30bb1e3c0c703af1a82eb1ad13b86ae5fcf28cee
vector-of-bool <vectorofbool@gmail.com>
2018-10-06 20:12:27 -0600

    Deploy 0.3.0

--- latest-info.cmake
+++ latest-info.cmake
@@ -1,12 +1,13 @@
 function(_pmm_changes version)
     if(PMM_VERSION VERSION_LESS version)
+        message(STATUS "[pmm]  - Changes in ${version}:")
         foreach(change IN LISTS ARGN)
-            message(STATUS "[pmm]   - ${change}")
+            message(STATUS "[pmm]     - ${change}")
         endforeach()
     endif()
 endfunction()
 
-set(PMM_LATEST_VERSION 0.2.0)
+set(PMM_LATEST_VERSION 0.3.0)
 
 if(PMM_VERSION VERSION_LESS PMM_LATEST_VERSION AND NOT PMM_IGNORE_NEW_VERSION)
     message(STATUS "[pmm] You are using PMM version ${PMM_VERSION}. The latest is ${PMM_LATEST_VERSION}.")
@@ -17,6 +18,9 @@ if(PMM_VERSION VERSION_LESS PMM_LATEST_VERSION AND NOT PMM_IGNORE_NEW_VERSION)
         "SETTINGS for Conan"
         "OPTIONS for Conan"
         "BUILD for setting the Conan install '--build' option"
+        )
+    _pmm_changes(0.3.0
+        "MSVC Support and Fixes"
         )
     message(STATUS "[pmm] To update, simply change the value of PMM_VERSION_INIT in pmm.cmake")
     message(STATUS "[pmm] You can disable these messages by setting PMM_IGNORE_NEW_VERSION to TRUE before including pmm.cmake")
