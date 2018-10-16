fd5c36e714a8c20bbe0a8072ce00dff8b9644512
vector-of-bool <vectorofbool@gmail.com>
2018-10-16 01:42:54 -0600

    Bump version 1.0.1

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ import conans
 
 class PMM(conans.ConanFile):
     name = 'pmm'
-    version = '1.0.0'
+    version = '1.0.1'
     settings = None
     exports_sources = '*'
     generators = 'cmake'
