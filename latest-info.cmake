7465ba6edb744563722cb22063ab579f18b54e88
vector-of-bool <vectorofbool@gmail.com>
2019-01-10 22:42:14 -0700

    Publish 1.3.0

--- latest-info.cmake
+++ latest-info.cmake
@@ -7,7 +7,7 @@ function(_pmm_changes version)
     endif()
 endfunction()
 
-set(PMM_LATEST_VERSION 1.2.0)
+set(PMM_LATEST_VERSION 1.3.0)
 
 if(PMM_VERSION VERSION_LESS PMM_LATEST_VERSION AND NOT PMM_IGNORE_NEW_VERSION)
     message(STATUS "[pmm] You are using PMM version ${PMM_VERSION}. The latest is ${PMM_LATEST_VERSION}.")
@@ -52,6 +52,15 @@ if(PMM_VERSION VERSION_LESS PMM_LATEST_VERSION AND NOT PMM_IGNORE_NEW_VERSION)
     _pmm_changes(1.2.0
         "Fix issues with finding Python in some Windows setups"
         "CMakeCM support"
+        )
+    _pmm_changes(1.3.0
+        "/Install, /Upgrade, and /Uninstall arguments to the script to support controlling PMM's Conan installation"
+        "pmm(CONAN) args: BINCRAFTERS and COMMUNITY to enable the Bincrafters and conan-community repositories, respectively"
+        "Generate a Conan profile file in the build directory for use by the user, instead of passing all command line args"
+        "Don't try to re-download already obtained files"
+        "Respect PYENV_ROOT when looking for Python"
+        "PMM DEBUG mode now prints all external commands that it executes"
+        "PMM DEBUG prints debugging information from vcpkg bootstrap"
         )
     message(STATUS "[pmm] To update, simply change the value of PMM_VERSION_INIT in pmm.cmake")
     message(STATUS "[pmm] You can disable these messages by setting PMM_IGNORE_NEW_VERSION to TRUE before including pmm.cmake")
