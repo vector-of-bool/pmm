caa6e051f09f4ba6d8b7e695d6261bc731a0b5f0
vector-of-bool <vectorofbool@gmail.com>
2019-12-10 18:13:31 -0700

    New copyright year

--- pmm.cmake
+++ pmm.cmake
@@ -1,6 +1,6 @@
 ## MIT License
 ##
-## Copyright (c) 2018 vector-of-bool
+## Copyright (c) 2019 vector-of-bool
 ##
 ## Permission is hereby granted, free of charge, to any person obtaining a copy
 ## of this software and associated documentation files (the "Software"), to deal
