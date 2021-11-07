2b0e5ec818a48f3841d8aa1352af40f072ae1a41
vector-of-bool <vectorofbool@gmail.com>
2021-11-06 18:30:17 -0600

    Stabilize docker tests with readonly mounts, and a more verbose test run script

--- tests/RunDockerTest.cmake
+++ tests/RunDockerTest.cmake
@@ -8,13 +8,8 @@ find_program(ARG_/Docker docker)
 
 set(test_pr_dir "${ARG_/ProjectDir}")
 
-if(NOT ARG_/Verbose)
-    set(OUTPUT_EAT_ARGS ERROR_VARIABLE out OUTPUT_VARIABLE out)
-endif()
-
 function(run_checked)
     execute_process(
-        ${OUTPUT_EAT_ARGS}
         RESULT_VARIABLE retc
         COMMAND ${ARGN}
         )
@@ -24,21 +19,16 @@ function(run_checked)
     endif()
 endfunction()
 
-message(STATUS "Configuring project")
 get_filename_component(pmm_dir "${CMAKE_CURRENT_LIST_DIR}/../pmm" ABSOLUTE)
 set(mount_args
-    -v "${test_pr_dir}:/host/source"
-    -v "${CMAKE_CURRENT_LIST_DIR}/../pmm:/host/pmm"
-    -v "${CMAKE_CURRENT_LIST_DIR}/../pmm.cmake:/host/pmm.cmake"
+    -v "${test_pr_dir}:/host/source:ro"
+    -v "${CMAKE_CURRENT_LIST_DIR}/../pmm:/host/pmm:ro"
+    -v "${CMAKE_CURRENT_LIST_DIR}/../pmm.cmake:/host/pmm.cmake:ro"
+    -v "${CMAKE_CURRENT_LIST_DIR}/..:/host/pmm-repo:ro"
     )
 run_checked(${ARG_/Docker} run --rm -t ${mount_args} ${ARG_/ContainerName}
-    ctest
-        --build-and-test /host/source /tmp/build
-        --build-generator "Unix Makefiles"
-        -VV
-        --build-options
-            -DPMM_URL=file:///host/pmm
-            -DPMM_INCLUDE=/host/pmm.cmake
-        --test-command
-            ctest --output-on-failure -j4
+    cmake -P /host/pmm-repo/tests/BuildTestProject.cmake
+        /SourceDir /host/source
+        /BinaryDir /tmp/build
+        /Generator "Unix Makefiles"
     )
